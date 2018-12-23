52e16d6761c691646d4c4a17af377add6ffc1910
BlanckeKenny <kenny.blancke@icloud.com>
2018-12-23 17:00:11 +0100

    user list sort

--- B04.EE.BlanckeK/B04.EE.BlanckeK/SqLite/UserSQLiteService.cs
+++ B04.EE.BlanckeK/B04.EE.BlanckeK/SqLite/UserSQLiteService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 using B04.EE.BlanckeK.Interfaces;
 using B04.EE.BlanckeK.Models;
@@ -25,7 +26,7 @@ namespace B04.EE.BlanckeK.SqLite
 
         public async Task<List<User>> GetAllUsers()
         {
-            return await Task.Run<List<User>>( () => Connection.Table<User>().ToList());
+            return await Task.Run<List<User>>( () => Connection.Table<User>().OrderByDescending(a => a.Level).OrderByDescending(a => a.Score).ToList());
         }
 
         public async Task DeleteUser(User user)
